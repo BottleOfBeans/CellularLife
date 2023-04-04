3d6b6a995879286abc0c4416660216b81e116eeb
Romir <romir.tandon@gmail.com>
2023-04-03 23:52:33 -0400

    I formatted all of the code and  documentation

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -4,92 +4,98 @@ import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class GameWindow extends JPanel  implements Runnable{
+public class GameWindow extends JPanel implements Runnable {
     /*
-        Game Tile Size shows the tile that each of the images on screen should be in pixels, this is not the amount that are displayed in each tile but rather the orignal one
-        Scalable value shows the value that is used to upscale the original images on the screen
-        Game Column and Row amounts show the amount of rows and columns that can be displayed on screen at one time
+     * Game Tile Size shows the tile that each of the images on screen should be in
+     * pixels, this is not the amount that are displayed in each tile but rather the
+     * orignal one
+     * Scalable value shows the value that is used to upscale the original images on
+     * the screen
+     * Game Column and Row amounts show the amount of rows and columns that can be
+     * displayed on screen at one time
      */
     static int GameTileSize = 16;
     static int ScalableValue = 3;
     static int gameColumnAmount = 24;
     static int gameRowAmount = 20;
 
-
     /*
-        ActualTileSize uses the variables from above to calculate how big each tile actually should be
-        Game Width and Height takes into account the columns and rows and calculates the window heights and widths using all the factors provided above
+     * ActualTileSize uses the variables from above to calculate how big each tile
+     * actually should be
+     * Game Width and Height takes into account the columns and rows and calculates
+     * the window heights and widths using all the factors provided above
      */
     static int ActualTileSize = GameTileSize * ScalableValue;
-    static int gameWidth = 1920; //gameColumnAmount*ActualTileSize;
-    static int gameHeight = 1080; //gameRowAmount*ActualTileSize;
+    static int gameWidth = 1920; // gameColumnAmount*ActualTileSize;
+    static int gameHeight = 1080; // gameRowAmount*ActualTileSize;
 
     /*
-        Offsets
-        These allow for the screen to always be focusing on the main body at all times :)
+     * Offsets
+     * These allow for the screen to always be focusing on the main body at all
+     * times :)
      */
     static double xOffset;
     static double yOffset;
 
-
     Thread gameThread;
 
-    //Game Values
+    // Game Values
     int FPS = 144;
     double zoomX = .2;
     double zoomY = .2;
 
-    //Creating the game windows and setting up the settings
-    public GameWindow(){
+    // Creating the game windows and setting up the settings
+    public GameWindow() {
         this.setPreferredSize(new Dimension(gameWidth, gameHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
         this.setFocusable(true);
     }
 
-    //Starting thread, managing frame updates
-    public void startWindowThread(){
+    // Starting thread, managing frame updates
+    public void startWindowThread() {
         gameThread = new Thread(this);
         gameThread.start();
     }
 
     /*
-        Particles!
+     * Particles!
      */
-    public double MaxParticle = 500;
-    public static ArrayList<Particle> particles = new ArrayList<Particle>();
+    public double MaxParticle = 500; // Maximum particle amount
+    public static ArrayList<Particle> particles = new ArrayList<Particle>(); // List containing all of the particles
 
-    //Loop that runs the thread, allows for it to sleep and start and ensures proper frame speed
+    // Loop that runs the thread, allows for it to sleep and start and ensures
+    // proper frame speed
     @Override
-    public void run(){
+    public void run() {
         /*
-            Add code here that runs before he game starts
+         * Add code here that runs before he game starts
          */
         Random rand = new Random();
-        
-        for(int i = 0; i < MaxParticle; i++){
-            double randomAngle = rand.nextInt(0,360);
+
+        // Creating a bunch of random particles with adjustment to zoom
+        for (int i = 0; i < MaxParticle; i++) {
+            double randomAngle = rand.nextInt(0, 360);
             double randomXPos = rand.nextDouble(0, gameWidth / zoomX);
             double randomYPos = rand.nextDouble(0, gameHeight / zoomY);
             particles.add(new Particle(new Vector2(randomAngle), new Vector2(randomXPos, randomYPos)));
         }
 
-
-        double drawInterval = 1000000000/FPS;
+        // Managing updates and FPS
+        double drawInterval = 1000000000 / FPS;
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
 
-
-        while(gameThread != null){
+        while (gameThread != null) {
 
             currentTime = System.nanoTime();
 
             delta += (currentTime - lastTime) / drawInterval;
 
             lastTime = currentTime;
-            if(delta >= 1){
-                update(delta);
+            if (delta >= 1) {
+                update(delta); // Sending the deltaTime through the update function
                 repaint();
                 delta--;
             }
@@ -97,40 +103,46 @@ public class GameWindow extends JPanel  implements Runnable{
         }
     }
 
-    public void update(double deltaTime){
-        for(Particle p : particles){
-            p.updateParticle(particles);
-            p.updateLocation(deltaTime);
+    public void update(double deltaTime) {
+        for (Particle p : particles) {
+            p.updateParticle(particles); // Updating particle location
+            p.updateLocation(deltaTime); // Updating particle change vector
         }
     }
 
-    //Function that paints the updated version of the frame {FPS} times a second.
-    public void paintComponent(Graphics g){
+    // Function that paints the updated version of the frame {FPS} times a second.
+    public void paintComponent(Graphics g) {
 
-        //Quick definition of varibles to use with the G2D library
+        // Quick definition of varibles to use with the G2D library
         super.paintComponent(g);
-        Graphics2D graphics = (Graphics2D)g;
+        Graphics2D graphics = (Graphics2D) g;
 
+        // Starting the transform process to zoom out
         AffineTransform oldTransform = graphics.getTransform();
-        graphics.scale(zoomX, zoomY);
+        graphics.scale(zoomX, zoomY); // Zoomgin on two axis'!
 
+        // Uncomment below code to visualize effect area
         // for(Particle p:particles){
-        //     graphics.setColor(Color.gray);
-        //     graphics.fill(p.getEffectArea());
+        // graphics.setColor(Color.gray);
+        // graphics.fill(p.getEffectArea());
         // }
 
-        for(Particle p : particles){
+        // Iterating through all of the particles and displaying them
+        for (Particle p : particles) {
+
             graphics.setColor(p.color);
             graphics.fill(p.getParticle());
-            //graphics.setColor(Color.BLACK);
-            //graphics.draw(p.directionLine());
+
+            // Uncomment below coode to get the direction lines visualized
+            // graphics.setColor(Color.BLACK);
+            // graphics.draw(p.directionLine());
         }
 
-        graphics.setTransform(oldTransform);
+        graphics.setTransform(oldTransform); // Putting the locations of the objects to what they should be post zoom.
 
-        //Stopping the use of the library to ensure that no more processing power than needed is used
+        // Stopping the use of the library to ensure that no more processing power than
+        // needed is used
         graphics.dispose();
     }
 
 }
-
--- src/Particle.java
+++ src/Particle.java
@@ -2,26 +2,22 @@ import java.awt.Color;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.util.ArrayList;
-import java.util.Random;
 
-import org.w3c.dom.css.RGBColor;
+public class Particle extends GameWindow {
 
-public class Particle extends GameWindow{
-
-    //Visual Effects
+    // Visual Effects
     double radius = 5;
 
-    //Game Speed Effects    
+    // Game Speed Effects
     double tickSpeed = 3;
     double speed = 10;
 
+    // Equation Variables
+    double effectradius = 55;
+    double rotationProportional = -6; // Beta
+    double fixedRotation = 120; // Alpha
 
-    //Equation Variables
-    double effectradius = 67;
-    double rotationProportional = -4; // Beta
-    double fixedRotation = 47; // Alpha
-    
-
+    // Various Constructor Values
     public Color color = Color.white;
     public int colorCode;
     public Vector2 changeVector;
@@ -29,80 +25,112 @@ public class Particle extends GameWindow{
     public double diameter;
     public double effectDiameter;
 
-    public Particle(Vector2 changeVector, Vector2 currentPos){
+    public Particle(Vector2 changeVector, Vector2 currentPos) {
+
+        // Taking in the changeVector
 
         this.changeVector = changeVector;
+
+        // Normalizing the vector to allow for direction alone and independent speed
+        // change
         changeVector = changeVector.normalize();
 
         this.currentPos = currentPos;
 
-        diameter = radius*2;
+        // Setting diameter of object + diameter of effect radius
+        diameter = radius * 2;
         effectDiameter = diameter + effectradius * 2;
 
+        // Placeholder Color
         this.color = Color.WHITE;
 
     }
 
-    public void updateLocation(double deltaTime){
-        currentPos.addVector(changeVector.returnMultiply(speed).returnDivide(deltaTime/tickSpeed));
+    // Updating location of particle
+    public void updateLocation(double deltaTime) {
+        // Updating location with regard to speed (multiplication) to time (deltaTime)
+        // and a set tickspeed (tickspeed)
+        currentPos.addVector(changeVector.returnMultiply(speed).returnDivide(deltaTime / tickSpeed));
     }
 
-    public Ellipse2D getParticle(){
-        return new Ellipse2D.Double(currentPos.x-radius, currentPos.y-radius, diameter,diameter);
+    // Various Get methods for location, object itself and effect areas.
+    public Ellipse2D getParticle() {
+        return new Ellipse2D.Double(currentPos.x - radius, currentPos.y - radius, diameter, diameter);
     }
 
-    public Line2D directionLine(){
-        return new Line2D.Double(currentPos.x, currentPos.y,changeVector.normalize().x * 30 + currentPos.x,changeVector.normalize().y * 30 +currentPos.y);
+    public Line2D directionLine() {
+        return new Line2D.Double(currentPos.x, currentPos.y, changeVector.normalize().x * 30 + currentPos.x,
+                changeVector.normalize().y * 30 + currentPos.y);
     }
 
-    public Ellipse2D getEffectArea(){
-        return new Ellipse2D.Double(currentPos.x - (effectradius + radius), currentPos.y - (effectradius + radius), effectDiameter, effectDiameter);
+    public Ellipse2D getEffectArea() {
+        return new Ellipse2D.Double(currentPos.x - (effectradius + radius), currentPos.y - (effectradius + radius),
+                effectDiameter, effectDiameter);
     }
 
-    public int sign(double number){
-        if(number > 0){
+    // Quick function that allows one to get the sign of a number
+    public int sign(double number) {
+        if (number > 0) {
             return 1;
-        }
-        else if(number == 0){
+        } else if (number == 0) {
             return 0;
-        }else{
+        } else {
             return -1;
         }
     }
 
-    public void updateColor(double number){
-        number = Math.abs(number*50) ;
+    // Updating the color of the particle based on its surroundings
+    public void updateColor(double number) {
+        number = Math.abs(number * 50);
         int RedValue = (int) Math.min(number, 255);
-        int BlueValue = (int) Math.max(0, 255-number/2);
-        int GreenValue = (int) Math.min(Math.max(0, number - 255/3),255);
+        int BlueValue = (int) Math.max(0, 255 - number / 2);
+        int GreenValue = (int) Math.min(Math.max(0, number - 255 / 5), 255);
 
         this.color = new Color(RedValue, GreenValue, BlueValue);
     }
 
-    public void updateParticle(ArrayList<Particle> particles){
-        double nNeighbors = 0;
-        double sNeighbors = 0;
+    // Updating the direction of the particle based on surrounding particles
+    public void updateParticle(ArrayList<Particle> particles) {
+        double nNeighbors = 0; // North Neighbors
+        double sNeighbors = 0; // South Neighbors
 
-        for(Particle p : particles){
-            Vector2 difference = new Vector2(this.currentPos.x - p.currentPos.x , this.currentPos.y - p.currentPos.y);
-            if(difference.magnitude() < effectDiameter){
-                double angle = Math.atan2(difference.x, difference.y);
-                if(angle >= 180 && angle <= 360 || angle <=90 && angle > 0){
-                    nNeighbors ++;
-                }else{
-                    sNeighbors ++;
+        // Looping through all of the particles
+        for (Particle p : particles) {
+            // Difference is the distance vector between the two particles
+            Vector2 difference = new Vector2(this.currentPos.x - p.currentPos.x, this.currentPos.y - p.currentPos.y);
+
+            // Checking if the distance is within the diameter
+            if (difference.magnitude() < effectDiameter) {
+                double angle = Math.atan2(difference.x, difference.y); // Getting the angle between the two objects
+                if (angle >= 180 && angle <= 360 || angle <= 90 && angle > 0) {
+                    nNeighbors++; // If within 180*-360* add to Northern neighbors
+                } else {
+                    sNeighbors++; // If within anything thelse add to Southern neighbors
                 }
             }
         }
 
-        double N = nNeighbors + sNeighbors;
+        double N = nNeighbors + sNeighbors; // The total number of neighbors that effect the experience
 
-        double newAngleRad = Math.toRadians(fixedRotation) + rotationProportional * N * sign(nNeighbors - sNeighbors);
-        
-        double newX = Math.cos(newAngleRad) * changeVector.x - Math.sin(newAngleRad) * changeVector.y;
-        double newY = Math.sin(newAngleRad) * changeVector.x + Math.cos(newAngleRad) * changeVector.y;
+        /*
+         * Using the equation (Delta Theta [Radians]) = alpha + beta * N * sign(N - S)
+         * Alpha - fixedrotation
+         * Beta - proportion of rotation
+         * N - total number of surrounding bodies
+         * sign (S-N) - the sign of the southern neighbors subtracted from north, allows
+         * for turning in correct directions
+         */
 
-        changeVector = new Vector2(newX, newY);
-        updateColor(N);
-    }                    
-} 
+        double newAngleRad = Math.toRadians(fixedRotation) + rotationProportional * N * sign(sNeighbors - nNeighbors); // The
+                                                                                                                       // Change
+                                                                                                                       // Angle
+
+        double newX = Math.cos(newAngleRad) * changeVector.x - Math.sin(newAngleRad) * changeVector.y; // Combined X
+                                                                                                       // Vector
+        double newY = Math.sin(newAngleRad) * changeVector.x + Math.cos(newAngleRad) * changeVector.y; // Conbined Y
+                                                                                                       // Vector
+
+        changeVector = new Vector2(newX, newY); // Updating the change Vector
+        updateColor(N); // Updating the color of the particle
+    }
+}
